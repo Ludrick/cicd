75f85cc64ea8db3e2510d8511276b1f1c5b9af5e
Luciano <luciano.fernandes@oowlish.com>
2025-02-03 11:05:58 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -11,43 +11,98 @@ terraform {
   required_version = ">= 1.2.0"
 
   backend "s3" {
-    bucket         = "cicdproj-tfstate"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "cicdproj-tfstate"
+    key     = "infra/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
 ###############################################################################
-# AWS Provider - references the variable declared in variables.tf
+# AWS Provider
 ###############################################################################
 provider "aws" {
   region = var.aws_region
 }
 
 ###############################################################################
-# Resource: S3 Bucket: cicdproj-calls-envx
+# Variables
+# (Alternatively, put these in variables.tf)
+###############################################################################
+/*
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region where resources will be created"
+}
+
+variable "env" {
+  type        = string
+  default     = "env4"
+  description = "Environment suffix to append to resource names"
+}
+*/
+
+###############################################################################
+# IAM Policy Document for Lambda trust
+###############################################################################
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+###############################################################################
+# IAM Role for Lambda
+###############################################################################
+resource "aws_iam_role" "lambda_exec_role_env" {
+  name               = "lambda-exec-role-s3-dynamo-${var.env}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+# Attach Managed Policies to that Role
+resource "aws_iam_role_policy_attachment" "attach_lambda_basic" {
+  role       = aws_iam_role.lambda_exec_role_env.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_full" {
+  role       = aws_iam_role.lambda_exec_role_env.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamodb_full" {
+  role       = aws_iam_role.lambda_exec_role_env.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+###############################################################################
+# S3 Bucket: "cicdproj-calls-<env>"
 ###############################################################################
 resource "aws_s3_bucket" "calls" {
   bucket = "cicdproj-calls-${var.env}"
 }
 
 ###############################################################################
-# S3 Objects to create "folders"
+# S3 Objects (prefixes)
 ###############################################################################
-resource "aws_s3_object" "landing_zone_prefix" {
+resource "aws_s3_bucket_object" "landing_zone_prefix" {
   bucket = aws_s3_bucket.calls.bucket
   key    = "landing_zone/"
 }
 
-resource "aws_s3_object" "processed_prefix" {
+resource "aws_s3_bucket_object" "processed_prefix" {
   bucket = aws_s3_bucket.calls.bucket
   key    = "processed/"
 }
 
-
 ###############################################################################
-# DynamoDB Table: cicdproj_calls_bronze_envx
+# DynamoDB Table: "cicdproj_calls_bronze_<env>"
 ###############################################################################
 resource "aws_dynamodb_table" "calls_bronze" {
   name         = "cicdproj_calls_bronze_${var.env}"
@@ -67,69 +122,46 @@ resource "aws_dynamodb_table" "calls_bronze" {
   }
 }
 
-
-resource "aws_lambda_function" "bronze_env4" {
-  function_name = "cicdproj_calls_bronze_env4"
-  role          = aws_iam_role.lambda_exec_role_env4.arn
+###############################################################################
+# Lambda Function
+###############################################################################
+resource "aws_lambda_function" "bronze" {
+  function_name = "cicdproj_calls_bronze_${var.env}"
+  role          = aws_iam_role.lambda_exec_role_env.arn
   handler       = "data.bronze.handler.lambda_handler"
   runtime       = "python3.12"
 
-  # Reference the zip file built in the pipeline. 
-  # Since the pipeline zips to the root of the repo, 
-  # the path in Terraform is "../function.zip" if you keep your code in "infra/"
+  # We'll build 'function.zip' in the pipeline, placed at the repo root 
   filename = "${path.module}/../function.zip"
-  
-  # Optional environment variables 
+
+  # If you'd like to set environment variables:
   # environment {
   #   variables = {
-  #     CICDENV = "env4"
+  #     CICDENV = var.env
   #   }
   # }
-  
-  # If you want to update code only on file hash changes:
-  # source_code_hash = filebase64sha256("${path.module}/../function.zip")
 }
 
-
-
-## LAMBDA ROLE
-
-resource "aws_iam_role" "lambda_exec_role_env4" {
-  name               = "lambda-exec-role-s3-dynamo-env4"
-  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
-}
-
-# attach policies like AmazonS3FullAccess, AmazonDynamoDBFullAccess, etc.
-resource "aws_iam_role_policy_attachment" "dynamo_s3" {
-  role       = aws_iam_role.lambda_exec_role_env4.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-# ...plus other attachments as needed...
-
-
-
-
-
-## LAMBDA NOTIFICATION
-
-# 1) Give S3 permission to invoke the Lambda
+###############################################################################
+# Lambda Permission: allow S3 to invoke the Lambda
+###############################################################################
 resource "aws_lambda_permission" "allow_s3_invoke" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.bronze_env4.arn
+  function_name = aws_lambda_function.bronze.arn
   principal     = "s3.amazonaws.com"
-  
-  # The bucket that's allowed to invoke
-  source_arn    = aws_s3_bucket.calls_env4.arn
+  source_arn    = aws_s3_bucket.calls.arn
   source_account = "599224842127"
 }
 
-# 2) S3 Notification
-resource "aws_s3_bucket_notification" "calls_env4_notification" {
-  bucket = aws_s3_bucket.calls_env4.id
+###############################################################################
+# S3 -> Lambda Notification
+###############################################################################
+resource "aws_s3_bucket_notification" "calls_notification" {
+  bucket = aws_s3_bucket.calls.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.bronze_env4.arn
+    lambda_function_arn = aws_lambda_function.bronze.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "landing_zone/"
     filter_suffix       = ".json"
