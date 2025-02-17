2406eb29fb88417de1e5307fcd92de85eda83128
Luciano Fernandes <luciano.fernandes@oowlish.com>
2025-02-17 19:00:16 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -158,6 +158,9 @@ resource "aws_lambda_function" "bronze" {
   role          = aws_iam_role.lambda_exec_role_env.arn
   handler       = "data.api.handler.lambda_handler"
   runtime       = "python3.12"
+
+  filename = "${path.module}/../function.zip"
+  source_code_hash = filebase64sha256("${path.module}/../function.zip")
   }
 
 ###############################################################################
